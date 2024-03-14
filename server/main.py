d3ef9817ee41c1a722bec01e0b6cdcd683e595a5
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-03-14 13:02:32 -0600

    Add API endpoints for events categories and events

--- server/main.py
+++ server/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from typing import Optional
+import json
 
 app = FastAPI()
+
+with open("data.json") as f:
+    events = json.load(f)
 
 
 @app.get("/")
@@ -8,6 +13,16 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/api/")
+async def say_hello():
+    return {"message": f"Hello user"}
+
+
+@app.get("/api/events-categories")
+def read_events_categories(skip: Optional[int] = 0, limit: Optional[int] = 10):
+    return events['events_categories'][skip: skip + limit]
+
+
+@app.get("/api/events")
+def read_events():
+    return events['allEvents']
